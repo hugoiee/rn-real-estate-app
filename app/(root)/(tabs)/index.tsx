e387ddefd880696da049c22cba8bce2e13da77c1
hugo <hugoiee@icloud.com>
2025-01-11 10:51:57 +0800

    feat(main): ✨️login pages

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import {Link} from "expo-router";
 import {Text, View} from "react-native";
 
 export default function Index() {
@@ -12,6 +13,9 @@ export default function Index() {
       <Text className="font-bold text-3xl my-10 font-rubik">
         Edit app/index.tsx to edit this screen.
       </Text>
+      <View>
+        <Link href={"../../sign-in"}>sign in</Link>
+      </View>
     </View>
   );
 }
